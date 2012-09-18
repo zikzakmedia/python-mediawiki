7c26732efa520e16c35350815ce98cd7610a0bcb
raimonesteve <raimonesteve@gmail.com>
2012-09-18 17:29:33 +0200

    Licence GNU General Public License (GPL)

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3', 
+        'License :: OSI Approved :: GNU General Public License (GPL)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
